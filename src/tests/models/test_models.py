c49bd5fbca33d48da4b0f5ec8aeaa7084047c559
jcosta86 <jeferson.costa@olist.com>
2021-01-28 14:48:05 -0300

    refatorando testes para testar instancia

--- src/tests/models/test_models.py
+++ src/tests/models/test_models.py
@@ -1,34 +1,20 @@
 import sys
 sys.path.append('.')
 from src.models.category import Category
+from src.models.base_model import BaseModel
 
-# class Category(BaseModel):
-#     __tablename__ = 'category'
-#     name = Column('name', String(length=100), nullable=False)
-#     description = Column('description', String(length=255), nullable=True)
+def test_category_model_instance():
+    name = 'name'
+    description = 'description'
 
-#     def __init__(self,name:str, description:str) -> None:
-#         self.name = name
-#         self.description =  description
-        
-#     @validates('name')
-#     def validate_name(self, key, name):
-#         if not isinstance(name, str):
-#             raise TypeError('Name is not string')
-#         if not name.strip():
-#             raise ValueError('Name can not be empty')
-#         if len(name) > 100:
-#             raise ValueError('Name can not more thank 100 characters')
-#         return name
+    cat = Category(name, description)
+
+    assert isinstance(cat, Category)
+    assert isinstance(cat, BaseModel)
     
-#     @validates('description')
-#     def validate_description(self, key, description):
-#         if not isinstance(description, str):
-#             raise TypeError('Description must be a string')
-#         if len(description) > 255:
-#             raise ValueError('Name can not more than 255 characters')
-
-
+    assert cat.name == name
+    assert cat.description == description
+    
 def test_name_is_not_none():
     try:
         category_test = Category(None, 'description')
