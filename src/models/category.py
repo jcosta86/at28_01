fc4e161c072ad06e4c6c5146fb72933eaaef90ba
jcosta86 <jeferson.costa@olist.com>
2021-01-28 13:55:23 -0300

    criando @validates para name e description de category

--- src/models/category.py
+++ src/models/category.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('.')
 from sqlalchemy import Column, String
+from sqlalchemy.orm import validates
 
 from src.models.base_model import BaseModel
 
@@ -13,3 +14,20 @@ class Category(BaseModel):
     def __init__(self,name:str, description:str) -> None:
         self.name = name
         self.description =  description
+        
+    @validates('name')
+    def validate_name(self, key, name):
+        if not isinstance(name, str):
+            raise TypeError('Name is not string')
+        if not name.strip():
+            raise ValueError('Name can not be empty')
+        if len(name) > 100:
+            raise ValueError('Name can not more thank 100 characters')
+        return name
+    
+    @validates('description')
+    def validate_description(self, key, description):
+        if not isinstance(description, str):
+            raise TypeError('Description must be a string')
+        if len(description) > 255:
+            raise ValueError('Name can not more than 255 characters')
