48564edd03f0dce3d14566917e2a4827c8e21a30
Derli Machado <dam.machado1@gmail.com>
2021-01-29 16:58:53 -0300

    refactoring tests to using raises.

--- src/tests/models/test_models.py
+++ src/tests/models/test_models.py
@@ -1,7 +1,9 @@
 import sys
+
 sys.path.append('.')
 from src.models.category import Category
 from src.models.base_model import BaseModel
+import pytest
 
 
 def test_category_model_instance():
@@ -12,57 +14,37 @@ def test_category_model_instance():
 
     assert isinstance(cat, Category)
     assert isinstance(cat, BaseModel)
-    
+
     assert cat.name == name
     assert cat.description == description
-    
-def test_name_is_not_none():
-    try:
-        category_test = Category(None, 'description')
-        raise AssertionError('The exception was not raised.')
-    except Exception as e:
-        assert isinstance(e, TypeError)
-        assert e.args == ('Name is not string',)
-    
-def test_name_is_a_string():
-    try:
-        category_test = Category(1.6, 'description')
-        raise AssertionError('The exception was not raised.')
-    except Exception as e:
-        assert isinstance(e, TypeError)
-        assert e.args == ('Name is not string',)
-       
-def test_empty_name():
-    try:
-        category_test = Category(''*5, 'description')
-        raise AssertionError('The exception was not raised.')
-    except Exception as e:
-        assert isinstance(e, ValueError)
-        assert e.args == ('Name can not be empty',)
 
-        
+
+def test_name_is_not_none():
+    with pytest.raises(TypeError):
+        category_test = Category(None, 'Melhor time do Brasil')
+
+
+def test_name_is_a_string():
+    with pytest.raises(TypeError):
+        category_test = Category(1.6, 'description')
+
+
+def test_empty_name():
+    with pytest.raises(ValueError):
+        category_test = Category('' * 5, 'description')
+
+
 def test_name_len():
-    try:
-        category_test = Category('Name'*101, 'description')
-        raise AssertionError('The exception was not raised.')
-    except Exception as e:
-        assert isinstance(e, ValueError)
-        assert e.args == ('Name can not more thank 100 characters',)
-                
- 
+    with pytest.raises(ValueError):
+        category_test = Category('Name' * 101, 'description')
+
+
 def test_description_is_a_string():
-    try:
+    with pytest.raises(TypeError):
         category_test = Category('name', 1.6)
-        raise AssertionError('The exception was not raised.')
-    except Exception as e:
-        assert isinstance(e, TypeError)
-        assert e.args == ('Description must be a string',)
+
 
 def test_description_len():
-    try:
-        category_test = Category('Name', 'description'*255)
+    with pytest.raises(ValueError):
+        category_test = Category('Name', 'description' * 255)
         raise AssertionError('The exception was not raised.')
-    except Exception as e:
-        assert isinstance(e, ValueError)
-        assert e.args == ('Name can not more than 255 characters',)
-      
