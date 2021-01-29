e38ed87726dd6630cd3ac2caff09d8cdbb18fa69
Derli Machado <dam.machado1@gmail.com>
2021-01-29 16:57:07 -0300

    Refac: changing attributes to using validator.

--- src/models/category.py
+++ src/models/category.py
@@ -1,4 +1,7 @@
 import sys
+
+from src.utils.validators import validate_type, validate_not_empty, validate_len
+
 sys.path.append('.')
 from sqlalchemy import Column, String
 from sqlalchemy.orm import validates
@@ -11,24 +14,18 @@ class Category(BaseModel):
     name = Column('name', String(length=100), nullable=False)
     description = Column('description', String(length=255), nullable=True)
 
-    def __init__(self,name:str, description:str) -> None:
+    def __init__(self, name: str, description: str) -> None:
         self.name = name
-        self.description =  description
+        self.description = description
         
+
     @validates('name')
     def validate_name(self, key, name):
-        if not isinstance(name, str):
-            raise TypeError('Name is not string')
-        if not name.strip():
-            raise ValueError('Name can not be empty')
-        if len(name) > 100:
-            raise ValueError('Name can not more thank 100 characters')
-        return name
-    
+        name = validate_type(name, str, key)
+        name = validate_not_empty(name, key)
+        return validate_len(name, 100, key)
+
     @validates('description')
     def validate_description(self, key, description):
-        if not isinstance(description, str):
-            raise TypeError('Description must be a string')
-        if len(description) > 255:
-            raise ValueError('Name can not more than 255 characters')
-        return description
+        description = validate_type(description, str, key)
+        return validate_len(description, 255, key)
