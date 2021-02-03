006b876d61d106860ffea9bcd1c3c76bfd0c64f5
Derli Machado <dam.machado1@gmail.com>
2021-02-03 14:47:00 -0300

    Add create all.

--- src/dao/session.py
+++ src/dao/session.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from src.models.base_model import BaseModel
 
 
 class Session:
@@ -10,9 +11,11 @@ class Session:
         password = 'ButecoOlist21'
         dbname = 'topskills17'
         self.__conn_string = f'{connector}://{user}:{password}@{host}:3306/{dbname}'
+        self.__engine = create_engine(self.__conn_string)
+        BaseModel.metadata.bind = self.__engine
+        BaseModel.metadata.create_all()
 
     def __enter__(self):
-        self.__engine = create_engine(self.__conn_string)
         Session = sessionmaker(self.__engine)
         self.__session = Session()
         return self.__session
