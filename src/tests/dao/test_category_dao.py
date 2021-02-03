5b730944e3bca8e0cbfd44a4f7dafc3d13b34df3
Derli Machado <dam.machado1@gmail.com>
2021-02-03 18:02:41 -0300

    refact: category dao.

--- src/tests/dao/test_category_dao.py
+++ src/tests/dao/test_category_dao.py
@@ -1,52 +1,58 @@
+
+import pytest
 from sqlalchemy.orm.exc import UnmappedInstanceError
+
+from src.dao.base_dao import BaseDao
 from src.dao.category_dao import CategoryDao
 from src.models.category import Category
-import pytest
 
 
 class TestCategoryDao:
     @pytest.fixture
     def create_instance(self):
-        cat = Category('Namecat', 'Namedes')
-        return cat
+        return CategoryDao()
 
-    def test_instance(self):
-        category_dao = CategoryDao()
-        assert isinstance(category_dao, CategoryDao)
+
+    def test_instance(self, create_instance):
+        assert isinstance(create_instance, CategoryDao)
+        assert isinstance(create_instance, BaseDao)
 
     def test_save(self, create_instance):
-        category_saved = CategoryDao().save(create_instance)
+        category = Category('nome', 'desc')
+        category_saved = create_instance.save(category)
 
         assert category_saved.id_ is not None
-        CategoryDao().delete(category_saved)
+        create_instance.delete(category_saved)
 
-    def test_not_save(self):
+    def test_not_save(self, create_instance):
         with pytest.raises(UnmappedInstanceError):
-            CategoryDao().save('category')
+            create_instance.save('category')
 
     def test_read_by_id(self, create_instance):
-        category_saved = CategoryDao().save(create_instance)
-        category_read = CategoryDao().read_by_id(category_saved.id_)
+        category = Category('nome', 'desc')
+        category_saved = create_instance.save(category)
+        category_read = create_instance.read_by_id(category_saved.id_)
 
         assert isinstance(category_read, Category)
         CategoryDao().delete(category_read)
 
-    def test_not_read_by_id(self):
+    def test_not_read_by_id(self, create_instance):
         with pytest.raises(TypeError):
-            CategoryDao().read_by_id('category_saved.id_')
+            create_instance.read_by_id('category_saved.id_')
 
-    def test_read_all(self):
-        category_read = CategoryDao().read_all()
+    def test_read_all(self, create_instance):
+        category_read = create_instance.read_all()
         assert isinstance(category_read, list)
 
     def test_delete(self, create_instance):
-        category_saved = CategoryDao().save(create_instance)
-        category_read = CategoryDao().read_by_id(category_saved.id_)
-        CategoryDao().delete(category_read)
-        category_read = CategoryDao().read_by_id(category_saved.id_)
+        category = Category('nome', 'desc')
+        category_saved = create_instance.save(category)
+        category_read = create_instance.read_by_id(category_saved.id_)
+        create_instance.delete(category_read)
+        category_read = create_instance.read_by_id(category_saved.id_)
 
         assert category_read is None
 
-    def test_not_delete(self):
+    def test_not_delete(self, create_instance):
         with pytest.raises(UnmappedInstanceError):
-            CategoryDao().delete('category_read')
+            create_instance.delete('category_read')
